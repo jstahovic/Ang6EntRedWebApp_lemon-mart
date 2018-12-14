3a0210b0846434a23666a11fe7a1b1ab453c871f
jstahovic <jstahovicjr@gmail.com>
2018-12-14 15:54:42 -0500

    Fixed author's code to allow the project to be built by adding the wiring necessary for the NavigationMenuComponent.

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
-import { ProfileComponent } from './profile/profile.component'
+import { RouterModule, Routes } from '@angular/router'
 import { LogoutComponent } from './logout/logout.component'
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
+import { ProfileComponent } from './profile/profile.component'
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'logout', component: LogoutComponent },
+  { path: 'navigation-menu', component: NavigationMenuComponent },
 ]
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
-import { UserRoutingModule } from './user-routing.module'
-import { ProfileComponent } from './profile/profile.component'
+import { NgModule } from '@angular/core'
 import { LogoutComponent } from './logout/logout.component'
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
+import { ProfileComponent } from './profile/profile.component'
+import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
   imports: [CommonModule, UserRoutingModule],
-  declarations: [ProfileComponent, LogoutComponent],
+  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
 })
 export class UserModule {}
